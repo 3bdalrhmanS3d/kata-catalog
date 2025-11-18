c3ebfa29d688d2a62fbe3ea2219abcb23d97d6e1
3bdalrhmanS3d <abdalrhmansaad24@gmail.com>
2025-11-18 21:04:09 +0200

    Implement balanced bracket checker using stack-based validation

---
+++ Balanced Brackets/BalancedBrackets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balanced_Brackets
+{
+    public class BalancedBrackets
+    {
+        public bool IsBalanced(string line)
+        {
+
+            if (string.IsNullOrEmpty(line))
+                return false;
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in line)
+            {
+                if (c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ']')
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    stack.Pop();
+                }
+
+            }
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Balanced Brackets/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Balanced_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string line = Console.ReadLine()!;
+
+            BalancedBrackets b = new BalancedBrackets();
+
+            Console.WriteLine(  b.IsBalanced( line ) );
+        }
+    }
+}
